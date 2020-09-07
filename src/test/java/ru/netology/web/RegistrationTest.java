435684c444c8476d89ad20d57d2e1656ac2703c1
Киселев Тимур <kiselev@mzta.ru>
2020-09-07 17:19:56 +0300

    начал делать 2 задачу

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -3,6 +3,8 @@ package ru.netology.web;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -23,25 +25,33 @@ class RegistrationTest {
 
         open("http://localhost:9999");
         LocalDate date = LocalDate.now();
-        int nextDay = date.getDayOfMonth() + 7;
+        String nextDay = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
         $$("[placeholder=\"Город\"]").last().setValue("Ка");
         $(withText("Красноярск")).click();
-        $$("[placeholder=\"Дата встречи\"]").last().setValue(String.valueOf(date));
-        $$("[class='calendar__row'] > [class='calendar__day']").find(exactText(String.valueOf(nextDay))).click();
-        $$("[name='name']").last().setValue("Фамилия");
-        $$("[name='phone']").last().setValue("+79169682127");
+        int currentDate;
+        $(By.cssSelector("[data-test-id=date]".toString());
+
+        $$("[class='calendar__row'] > [class='calendar__day']").find(exactText(String.valueOf(date.getDayOfMonth()))).click();
+
+
+
+        $("[data-test-id=name] input").setValue("Фамилия и Имя");
+        $("[data-test-id=phone] input").setValue("+79169682127");
         $("[class='checkbox__box']").click();
         $$("button").find(exactText("Забронировать")).click();
         $(withText("Встреча успешно забронирована")).waitUntil(visible, 11000);
+        $(withText(nextDay)).waitUntil(visible, 11000);
     }
 
     @Test
     void shouldRegisterByCardNumber() {
         open("http://localhost:9999");
+
         $("[data-test-id=city] input").setValue("Красноярск");
         LocalDate dateDay = LocalDate.now().plusDays(8);
         String dateToEnter = dateDay.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-        $$("[placeholder=\"Дата встречи\"]").last().setValue(String.valueOf(dateDay));
+        $("[placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] input").setValue(String.valueOf(dateDay));
         $$("[class='calendar__row'] > [class='calendar__day']").find(exactText(String.valueOf(dateDay.getDayOfMonth()))).click();
         $("[data-test-id=name] input").setValue("Фамилия и Имя");
         $("[data-test-id=phone] input").setValue("+79169682127");
