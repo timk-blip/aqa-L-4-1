67eec4978ec10d88505c32a6c2561d8ed14b3830
Киселев Тимур <kiselev@mzta.ru>
2020-09-14 13:50:53 +0300

    change task_2

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,14 +1,16 @@
 package ru.netology.web;
+
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
+
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
-
 
 class RegistrationTest {
 
@@ -17,6 +19,61 @@ class RegistrationTest {
         Selenide.closeWebDriver();
     }
 
+    @Test
+    void shouldComplexElements() {
+
+        open("http://localhost:9999");
+        $$("[placeholder=\"Город\"]").last().setValue("Ка");
+        $(withText("Красноярск")).click();
+        //////////////////////////////////////////////////////////////////////
+        LocalDate date = LocalDate.now();
+        String currentDay = date.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        int month = date.getMonthValue();
+        int year = date.getYear();
+
+
+        String nextDate = "15.04.2022";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        LocalDate dateTime = LocalDate.parse(nextDate, formatter);
+      /* LocalDate dateTime = null;
+        dateTime.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        dateTime = LocalDate.parse(nextDate);
+        //String nextDate = "15.04.22";*/
+
+
+        int nextDay = dateTime.getDayOfMonth();
+        int nextMonth = dateTime.getMonthValue();
+        int nextYear = dateTime.getYear();
+
+        int resultYear = 0;
+        if (year <= nextYear) {
+            resultYear = nextYear - year;
+        }
+        int resultMonth = 0;
+        if (nextMonth >= month) {
+            resultMonth = nextMonth - month;
+        } else {
+            resultYear--;
+            resultMonth = nextMonth - month + 12;
+        }
+
+
+        $("[class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
+        for (int i = 0; i < resultYear; i++) {
+            $("[class='calendar__arrow calendar__arrow_direction_right calendar__arrow_double']").click();
+        }
+        for (int i = 0; i < resultMonth; i++) {
+            $("[class='calendar__arrow calendar__arrow_direction_right']").click();
+        }
+        $$("[class='calendar__row'] > [role='gridcell']").find(exactText(String.valueOf(nextDay))).click();
+        //////////////////////////////////////////////////////////////////////
+        $("[data-test-id=name] input").setValue("Киселев Тимур");
+        $("[data-test-id=phone] input").setValue("+79169682127");
+        $("[class='checkbox__box']").click();
+        $$("button").find(exactText("Забронировать")).click();
+        $(withText("Встреча успешно забронирована")).waitUntil(visible, 11000);
+        $(withText(nextDate)).waitUntil(visible, 11000);
+    }
 
     @Test
     void shouldRegisterByCardNumber() {
